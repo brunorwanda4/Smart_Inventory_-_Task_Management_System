af85b77ff8654e6d9f19eb4bc8838c39a856e87e
brunorwanda4 <brunorwanda4@gmail.com>
2025-05-17 10:26:53 +0200

    make get all users

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const mysql = require("mysql2");
-const bcrypt = require("bcrypt")
+const bcrypt = require("bcrypt");
 
 const app = express();
 
@@ -26,34 +26,68 @@ app.get("/", (req, res) => {
 
 // ----------------- users  ---------------------------
 
+app.post("/api/users", (req, res) => {
+  const { name, email, password, role } = req.body;
 
-app.post("/api/users" , (req , res) => {
-    const {name , email, password , role } = req.body
+  if (!name || !email || !password || !role) {
+    return res
+      .status(400)
+      .json({ message: "name , email, password , role, are required" });
+  }
 
-    if (!name || !email|| !password || !role) {
-        return res.status(400).json({message : "name , email, password , role, are required"})
+  if (role !== "Admin" && role !== "Manager" && role !== "Staff") {
+    return res.status(400).json({
+      message: "role is not supported choose : Admin, Manager and Staff",
+    });
+  }
+
+  db.query("SELECT * FROM users WHERE email = ?", [email], (getErr, getRes) => {
+    if (getErr) {
+      return res
+        .status(500)
+        .json({ message: "Get email error", error: getErr.message });
     }
 
-    if (role !== "Admin" && role !== "Manager" && role !== "Staff") {
-        return res.status(400).json({message : "role is not supported choose : Admin, Manager and Staff"})
+    if (getRes.length !== 0) {
+      return res.status(400).json({ message: "Email is ready exits " });
     }
 
-    db.query("SELECT * FROM users WHERE email = ?", [email] , (getErr , getRes) => {
-        if (getErr) {return res.status(500).json({message : "Get email error", error : getErr.message})}
+    bcrypt.hash(password, 10, (hashErr, hashRes) => {
+      if (hashErr) {
+        return res
+          .status(500)
+          .json({ message: "hash error", error: hashErr.message });
+      }
 
-        if (getRes.length !== 0) {return res.status(400).json({message : "Email is ready exits "})}
+      db.query(
+        "INSERT INTO users (name , email, password , role) VALUES (?, ?, ?, ?)",
+        [name, email, hashRes, role],
+        (insertErr, insertRes) => {
+          if (insertErr) {
+            return res
+              .status(500)
+              .json({ message: "Insert error", error: insertErr.message });
+          }
 
-        bcrypt.hash(password , 10, (hashErr , hashRes) => {
-            if (hashErr){ return res.status(500).json({message : "hash error", error : hashErr.message})}
-            
-            db.query("INSERT INTO users (name , email, password , role) VALUES (?, ?, ?, ?)", [name , email, hashRes , role ], (insertErr, insertRes) => {
-                if (insertErr) {return res.status(500).json({message : "Insert error", error : insertErr.message})}
-
-                return res.status(201).json({message : "User created 🌻", id : insertRes.insertId})
-            })
-        })
-        
-    })
+          return res
+            .status(201)
+            .json({ message: "User created 🌻", id: insertRes.insertId });
+        }
+      );
+    });
+  });
 });
+
+app.get("/api/users", (req, res) => {
+  db.query("SELECT * FROM users", (getErr, getRes) => {
+    if (getErr) {
+      return res
+        .status(500)
+        .json({ message: "Get all users error", error: getErr.message });
+    }
+    return res.status(200).json(getRes);
+  });
+});
+
 
 app.listen(3011, () => console.log("Server is run on port 3011"));
